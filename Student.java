6a5feb19a163473a5b9f6b3f10add98c1317739d
BishalParajuli2057 <bishalparajuli2057@gmail.com>
2024-03-04 14:27:09 +0200

    1

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student implements Printinfo {
+    private String name;
+    private String id;
+
+    public Student(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public String getInformation() {
+        return "Student Name: " + name + ", ID: " + id;
+    }
+}
